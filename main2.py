bdfd2a16d9883511459d86ea1a6f45ff98d40021
DANIILDISA <126347297+DANIILDISA@users.noreply.github.com>
2023-03-28 22:51:15 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,15 +1,23 @@
 import telebot
 from config import TOKEN, keys
 from extensions import ConvertionException, CryptoConverter
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 bot = telebot.TeleBot(TOKEN)
 
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)  # определяем клавиатуру
+start_button = KeyboardButton('/start')
+values_button = KeyboardButton('/values')
+keyboard.add(start_button, values_button)
 
-@bot.message_handler(commands=['start', 'help'])
+
+@bot.message_handler(commands=['start', 'help'])  # добавляем клавиатуру в стартовое сообщение
 def help(message: telebot.types.Message):
-    text = ('<Имя валюты>\n<имя валюты, в которую вы хотите конвертировать>\n<количество переводимой валюты>\n'
+    text = ('Использовать бота:\n'
+            '<Имя валюты>\n<имя валюты, в которую вы хотите конвертировать>\n'
+            '<количество переводимой валюты>\n\n'
             'Вы можете увидеть список всех доступных валют, введя команду /values')
-    bot.reply_to(message, text)
+    bot.reply_to(message, text, reply_markup=keyboard)
 
 
 @bot.message_handler(commands=['values'])
@@ -29,9 +37,9 @@ def convert(message: telebot.types.Message):
         quote, base, amount = map(str.lower, values)
         total_base = CryptoConverter.convert(quote, base, amount)
     except ConvertionException as e:
-        bot.reply_to(message, f'Ошибка пользователя:\n{e}')
+        bot.reply_to(message, f'Ошибка пользователя:\n{e}', reply_markup=keyboard)
     except Exception as e:
-        bot.reply_to(message, f'Не удалось обработать команду:\n{e}')
+        bot.reply_to(message, f'Не удалось обработать команду:\n{e}', reply_markup=keyboard)
     else:
         text = f'Цена {amount} {quote.capitalize()} в {base.capitalize()} - {total_base}'
         bot.send_message(message.chat.id, text)
